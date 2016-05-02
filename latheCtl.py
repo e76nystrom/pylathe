72170abe3da8f2b063c386d222be866ac37d1a8c
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-02 07:24:02 -0400

    "latheCtl.py Modified on 5/2/2016 at 7:24:01 AM"

--- latheCtl.py
+++ latheCtl.py
@@ -222,7 +222,9 @@ class AccelPlot(Accel):
             accelClocks = 0
             accel = False
 
-        print ("incr1 %d incr2 %d inc %d" % (incr1, incr2, accel))
+        print ("synAccel %d accelClocks %d accelSum %d" %
+               (synAccel, accelClocks, synAccel * accelClocks))
+        print ("incr1 %d incr2 %d sum %d" % (incr1, incr2, sum))
         stdout.flush()
         aClk = accelClocks
         aclSteps = 0
