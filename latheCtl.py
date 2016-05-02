107b19ea06e8d5b38749c0e28f35cba06aa564bc
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-02 17:29:54 -0400

    "latheCtl.py Modified on 5/2/2016 at 5:29:53 PM"

--- latheCtl.py
+++ latheCtl.py
@@ -1094,8 +1094,6 @@ else:
         accel = Test(True)
         accel.encoder = 2540 * 8
         accel.testNoAccelSetup(2540 * 8, 600)
-        accel.accel = 8
-        accel.accelClocks = 100
         accel.setDbgPrint(True)
         if testAxis = 'z':
             accel.zTestSync(arg1, arg2, arg3)
