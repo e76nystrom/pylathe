c81ddace703eb8364c7a8b9adf60b04604518911
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-03 18:51:40 -0400

    "latheCtl.py Modified on 5/3/2016 at 6:51:39 PM"

--- latheCtl.py
+++ latheCtl.py
@@ -654,11 +654,11 @@ class Test(Accel):
         self.xTestAccelCheck(None)
 
     def resetAll(self):
-        setXReg('XLDDCTL', 0)    # disable debug mode
-        setXReg('XLDTCTL', 0)    # clear taper
-        setXReg('XLDZCTL', ZRESET) # reset z
-        setXReg('XLDZCTL', 0)      # clear z mode
         setXReg('XLDXCTL', XRESET) # reset x
+        setXReg('XLDZCTL', ZRESET) # reset z
+        setXReg('XLDTCTL', 0)      # clear taper
+        setXReg('XLDDCTL', 0)      # disable debug mode
+        setXReg('XLDZCTL', 0)      # clear z mode
         setXReg('XLDXCTL', 0)      # clear x mode
 
     def testInit(self, encoder, dbgFreq=10000, dbgCount=4):
