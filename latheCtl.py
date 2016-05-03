49678eb4752cfdd5d1ee764a4b0ff7205742096c
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-03 19:52:22 -0400

    "latheCtl.py Modified on 5/3/2016 at 7:52:20 PM"

--- latheCtl.py
+++ latheCtl.py
@@ -787,8 +787,10 @@ class Test(Accel):
         if val & 2:
             setXReg('XLDXCTL', 0)   # clear x done flag
 
+        comm.xDbgPrint = True
         zVal = getXReg('XRDZXPOS')
         xVal = getXReg('XRDXXPOS')
+        comm.xDbgPrint = False
         maxClocks = max(zVal, xVal)
 
         setXReg('XLDZCTL', ZRESET) # reset z
