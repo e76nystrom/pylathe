d27ffaa7eb7f8543d364bad243e19cbc6c4a7909
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-09 17:36:33 -0400

    "latheCtl.py Modified on 5/9/2016 at 5:36:32 PM"

--- latheCtl.py
+++ latheCtl.py
@@ -260,6 +260,10 @@ class Accel():
             dyDeltaC = intIncPerClock * accel.accelClocks
             err = int(abs(dyDelta - dyDeltaC)) >> scale
             accel.dyIni = accel.dyMax - intIncPerClock * accel.accelClocks
+            if self.prt:
+                printf("dyIni %d dyMax %d intIncPerClock %d accelClocks %d" %
+                       (accel.dyIni, acccel.dyMax, intIncPerClock,
+                        accel.accelClocks))
             bits = bitSize(accel.dx) + 1
             if self.prt:
                 print ("dyIni %d dyMax %d dyDelta %d incPerClock %6.2f "\
