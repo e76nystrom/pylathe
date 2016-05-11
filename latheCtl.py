d7712351ca47cdd4c810e7ecdf5bfee00ce94162
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-11 11:59:44 -0400

    "latheCtl.py Modified on 5/11/2016 at 11:59:44 AM"

--- latheCtl.py
+++ latheCtl.py
@@ -185,10 +185,10 @@ class Turn():
                 print ("stepsSec2 %0.0f accelTime %8.6f accelClocks %d" %
                        (stepsSec2, accel.accelTime, accelClocks))
 
-            accelMinStep = int(((float(stepsSecMin) / stepsSec2) *
-                                stepsSecMin) / 2.0)
-            accelMaxStep = int(((float(stepsSecMax) / stepsSec2) *
-                                stepsSecMax) / 2.0)
+            accelMinStep = ((float(stepsSecMin) / stepsSec2) *
+                                stepsSecMin) / 2.0
+            accelMaxStep = ((float(stepsSecMax) / stepsSec2) *
+                                stepsSecMax) / 2.0
             accel.accelSteps = accelMaxStep - accelMinStep
             if self.prt:
                 print ("accelSteps %d accelMinStep %d accelMaxStep %d" %
