cd090f49e31415f5f2e5bd35c675b4c6e90c2867
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-11 06:57:12 -0400

    "latheCtl.py Modified on 5/11/2016 at 6:57:10 AM"

--- latheCtl.py
+++ latheCtl.py
@@ -1511,7 +1511,7 @@ else:
         command('CMD_ZSYNSETUP');
 
     if testId == 13:
-        for pitch in (.042, 0.050, .075, 0.1, 0.2):
+        for pitch in (.035, 0.050, .075, 0.1, 0.2):
             print "turn %0.3f\n" % (pitch)
             tmp = Turn(axis, minAccel, encoder, dbgPrint)
             accel = Test(axis, dbgClock, dbgPrint)
