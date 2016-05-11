7ac66d4e8d86ba74af9c87efb44a2f291fa6d44b
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-11 08:42:15 -0400

    "latheCtl.py Modified on 5/11/2016 at 8:42:13 AM"

--- latheCtl.py
+++ latheCtl.py
@@ -1532,13 +1532,5 @@ else:
             tmp.setup(accel, minAccel, float(i))
             print
 
-    if testId == 15:
-        print "turn %0.3f\n" % (pitch)
-        tmp = Turn(axis, minAccel, encoder, dbgPrint)
-        accel = Test(axis, dbgClock, dbgPrint)
-        tmp.setup(accel, rpm, pitch)
-        for accelClocks in (1000, 1500, 2000, 2500, 5000, 10000, 20000):
-            accel.setup(accelClocks)
-
 if not (comm.ser is None):
     comm.ser.close()
