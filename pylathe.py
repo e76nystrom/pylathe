24ef0b531a4868389a6c657ca0eb792d52012d91
Eric Nystrom <eric.nystrom@yahoo.com>
2016-05-22 16:39:40 -0400

    "pylathe.py Modified on 5/22/2016 at 4:39:39 PM"

--- pylathe.py
+++ pylathe.py
@@ -198,9 +198,9 @@ def getFloatInfo(key):
         tmp = info[key]
         try:
             return(float(tmp.GetValue))
-        except ValueError e:
+        except ValueError as e:
             return(0.0)
-    except KeyError e:
+    except KeyError as e:
         print "invalid key %s" % (key)
         stdout.flush()
 
