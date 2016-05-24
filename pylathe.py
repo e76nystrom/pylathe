1070868ed2676e28396cab998b19a41cf4b6a831
e76nystrom <eric.nystrom@yahoo.com>
2016-05-23 20:32:43 -0400

    "pylathe.py Modified on 5/23/2016 at 8:32:41 PM"

--- pylathe.py
+++ pylathe.py
@@ -364,7 +364,7 @@ def sendSpindleData(send=False):
                 setParm('SPIN_MIN_RPM', parmValue('spMinRPM'))
                 setParm('SPIN_MAX_RPM', parmValue('spMaxRPM'))
                 setParm('SPIN_ACCEL_TIME', parmValue('spAccelTime'))
-                setParm('SPIN_DIR_FLAG', parmValueBool('spinInvDir'))
+                setParm('SPIN_DIR_FLAG', parmValueBool('spInvDir'))
 
             command('CMD_SPSETUP')
             spindleDataSent = True
@@ -394,7 +394,7 @@ def sendZData(send=False):
             setParm('Z_JOG_MIN', parmValue('zJogMin'))
             setParm('Z_JOG_MAX', parmValue('zJogMax'))
 
-            setParm('Z_DIR_FLAG', str(int(parmValue('zInvDir'))))
+            setParm('Z_DIR_FLAG', parmValueBool('zInvDir'))
 
             command('CMD_ZSETUP')
             zDataSent = True
@@ -424,7 +424,7 @@ def sendXData(send=False):
             setParm('X_JOG_MIN', parmValue('xJogMin'))
             setParm('X_JOG_MAX', parmValue('xJogMax'))
 
-            setParm('X_DIR_FLAG', str(int(parmValue('xInvDir'))))
+            setParm('X_DIR_FLAG', parmValueBool('xInvDir'))
 
             command('CMD_XSETUP')
             xDataSent = True
@@ -2623,7 +2623,7 @@ class ZDialog(wx.Dialog):
             ("Max Speed", "zMaxSpeed"),
             ("Jog Min", "zJogMin"),
             ("Jog Max", "zJogMax"),
-            ("bInvert Dir", 'spinInvDir'))
+            ("bInvert Dir", 'zInvDir'))
         fieldList(self, sizerG, self.fields)
 
         sizerV.Add(sizerG, flag=wx.LEFT|wx.ALL, border=2)
