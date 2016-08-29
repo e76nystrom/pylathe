ed7561c53936a2eb1ac3668368eb8ad0aefc886a
Eric Nystrom <eric.nystrom@yahoo.com>
2016-08-29 18:58:42 -0400

    "pylathe.py Modified on 8/29/2016 at 6:58:41 PM"

--- pylathe.py
+++ pylathe.py
@@ -1971,7 +1971,7 @@ class JogPanel(wx.Panel):
         btn.Bind(wx.EVT_BUTTON, self.OnStartSpindle)
         sizerH.Add(btn, flag=wx.CENTER|wx.ALL, border=2)
 
-        sizerV.Add(sizerH, flag=wx.ALIGN_RIGHT|wx.CENTER|wx.ALL, border=2)
+        sizerV.Add(sizerH, flag=wx.CENTER|wx.ALL, border=2)
 
         self.SetSizer(sizerV)
         sizerV.Fit(self)
