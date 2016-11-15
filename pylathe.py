fa193d8e622c062e3ead0c458dd2017f6cf8d55b
Eric Nystrom <eric.nystrom@yahoo.com>
2016-11-15 05:32:11 -0500

    "pylathe.py Modified on 11/15/2016 at 5:32:09 AM"

--- pylathe.py
+++ pylathe.py
@@ -3261,7 +3261,7 @@ class MainFrame(wx.Frame):
         allHids = find_all_hid_devices()
         if allHids:
             for index, device in enumerate(allHids):
-                print index, device
+                print index, device.product_name
         openSerial(getInfo('commPort'), 57600)
         global cmds, parms
         comm.cmds = cmds
