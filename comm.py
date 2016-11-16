ab9aaa141086897c5f0322c4e2aaed825c76157b
Eric Nystrom <eric.nystrom@yahoo.com>
2016-11-16 03:45:08 -0500

    "comm.py Modified on 11/16/2016 at 3:45:07 AM"

--- comm.py
+++ comm.py
@@ -99,6 +99,9 @@ def queParm(parm, val):
             valString = "0"
     # cmd = '\x01%x %x %s ' % (cmds['LOADVAL'][0], parmIndex, valString)
     cmd = ' %x %s' % (parmIndex, valString)
+    if True: # xDbgPrint:
+        print("%-15s %s" % (parm, cmd.strip()))
+        stdout.flush()
     length = len(cmd)
     if cmdLen + length > 80:
         sendMulti()
