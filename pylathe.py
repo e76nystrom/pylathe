3202fc932fd64ab9cbd218e789342a40477b02ba
Eric Nystrom <eric.nystrom@yahoo.com>
2017-07-24 04:59:23 -0400

    "pylathe.py Modified on 7/24/2017 at 4:59:19 AM"

--- pylathe.py
+++ pylathe.py
@@ -1401,6 +1401,7 @@ class Taper(UpdatePass):
         self.zStart = getFloatVal(tp.zStart)
         self.zLength = getFloatVal(tp.zLength)
         self.zEnd = self.zStart + self.zLength
+        self.zLength = abs(self.zLength)
         self.zFeed = abs(getFloatVal(tp.zFeed))
         self.zRetract = abs(getFloatVal(tp.zRetract))
 
