05c92f3114a3b9852569a238773f44572148519e
Eric Nystrom <eric.nystrom@yahoo.com>
2017-08-15 19:13:40 -0400

    "pylathe.py Modified on 8/15/2017 at 7:13:37 PM"

--- pylathe.py
+++ pylathe.py
@@ -4229,22 +4229,22 @@ class MainFrame(wx.Frame):
 
         readInfo(configFile)
 
-        vars = (('zSvHomeOffset', zHomeOffset), \
-                ('xSvHomeOffset', xHomeOffset))
+        vars = (('zSvHomeOffset', 'zHomeOffset'), \
+                ('xSvHomeOffset', 'xHomeOffset'))
         if DRO:
-            vars += (('zSvDROOffset', zDROOffset), \
-                     ('xSvDROOffset', xDROOffset), \
-                     ('zSvDROPosition', zDROPostion), \
-                     ('xSvDROPosition', xDROPositon))
+            vars += (('zSvDROOffset', 'zDROOffset'), \
+                     ('xSvDROOffset', 'xDROOffset'), \
+                     ('zSvDROPosition', 'zDROPostion'), \
+                     ('xSvDROPosition', 'xDROPositon'))
 
         for (key, var) in vars:
-            # exec('global ' + var)
+            exec('global ' + var)
             if not key in info:
-               newInfo(key, "%0.4f" % (var))
+               newInfo(key, "%0.4f" % (eval(var)))
             else:
                 exp = key + ' = getFloatInfo(\'' + var + '\')'
                 exec(exp)
-                print("%s = %s" % (key, var))
+                print("%s = %s" % (key, eval(var)))
             stdout.flush()
 
         dw, dh = wx.DisplaySize()
