f74043742155099575811452939d2b45bb793d4d
Eric Nystrom <eric.nystrom@yahoo.com>
2017-08-18 12:09:01 -0400

    "pylathe.py Modified on 8/18/2017 at 12:08:57 PM"

--- pylathe.py
+++ pylathe.py
@@ -4071,7 +4071,7 @@ class UpdateThread(Thread):
                             dbg.flush()
                             (cmd, val) = result.split(' ')[:2]
                             try:
-                                cmd = int(val, 16)
+                                cmd = int(cmd, 16)
                                 val = int(val, 16)
                                 try:
                                     action = dbgTbl[cmd]
