fc2b331798b05b10f168f1ad84eefcf2ed7f7b91
Eric Nystrom <eric.nystrom@yahoo.com>
2017-08-20 15:10:16 -0400

    "xxtest.py Modified on 8/20/2017 at 3:10:13 PM"

--- xxtest.py
+++ xxtest.py
@@ -7,7 +7,7 @@ for i in range(20):
     x = i * xInc
     diff = x - xLast
     print("x %7.4f xLast %7.4f diff %7.4f" % (x, xLast, diff))
-    if diff < h:
+    if diff > h:
         x = xLast + h
         print("x %7.4f" % (x))
     xLast = x
