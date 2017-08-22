f71fcb586c225b934c3ace7ca0d46cfc556c89a0
Eric Nystrom <eric.nystrom@yahoo.com>
2017-08-22 16:35:08 -0400

    "xxtest.py Modified on 8/22/2017 at 4:35:05 PM"

--- xxtest.py
+++ xxtest.py
@@ -4,20 +4,21 @@ class Test():
     def __init__(self):
         print(hasattr(self, "t1"))
         print(callable(self.t1))
+        print(callable(self.t2))
 
     def t1(self):
         pass
 
 a = Test()
 
-h = -0.005
-xInc = -0.0025
-lastX = 10
-for i in range(20):
-    x = i * xInc
-    diff = x - lastX
-    print("x %7.4f lastX %7.4f diff %7.4f" % (x, lastX, diff))
-    if diff > h:
-        x = lastX + h
-        print("x %7.4f" % (x))
-    lastX = x
+# h = -0.005
+# xInc = -0.0025
+# lastX = 10
+# for i in range(20):
+#     x = i * xInc
+#     diff = x - lastX
+#     print("x %7.4f lastX %7.4f diff %7.4f" % (x, lastX, diff))
+#     if diff > h:
+#         x = lastX + h
+#         print("x %7.4f" % (x))
+#     lastX = x
