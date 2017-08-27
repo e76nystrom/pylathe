e68617b907b4935b24df39b34915898451eb7004
Eric Nystrom <eric.nystrom@yahoo.com>
2017-08-27 08:04:38 -0400

    "pylathe.py Modified on 8/27/2017 at 8:04:34 AM"

--- pylathe.py
+++ pylathe.py
@@ -2293,8 +2293,8 @@ class ScrewThread(UpdatePass):
         if self.panel.firstFeedBtn.GetValue():
             firstWidth = 2 * self.firstFeed * self.tanAngle
             self.areaPass = 0.5 * self.firstFeed * firstWidth
-            print("firstFeed %6.4f firstWidth %6.4f firstArea %8.6f" % \
-                  (self.firstFeed, firstWidth, firstArea))
+            print("firstFeed %6.4f firstWidth %6.4f areaPass %8.6f" % \
+                  (self.firstFeed, firstWidth, self.areaPass))
         else:
             lastDepth = self.depth - self.lastFeed
             lastArea = (lastDepth * lastDepth) * self.tanAngle
