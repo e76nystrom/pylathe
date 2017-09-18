074867963eccb2a9f3eca2b6e381c5dca6b739a8
e76nystrom <eric.nystrom@yahoo.com>
2017-09-18 13:30:08 -0400

    "configInfo.py Modified on 9/18/2017 at 1:30:06 PM"

--- configInfo.py
+++ configInfo.py
@@ -3,6 +3,7 @@ from sys import stdout
 
 info = []
 infoData = []
+configTable = None
 
 class InfoValue():
     def __init__(self, val):
@@ -166,7 +167,8 @@ def getFloatInfo(key):
         try:
             return(float(val))
         except ValueError:
-            print("getFloatInfo ValueError key %d %s" % (key, val))
+            print("getFloatInfo ValueError key %d %s %s" % \
+                  (key, configTable[key], val))
     except KeyError:
         print("getFloatInfo KeyError %d" % (key))
     stdout.flush()
